a1c1c0d20afdac3ef093b9b0f8ac2a4b47c3b2db
klxcoder <klxcoder@gmail.com>
2025-04-05 19:59:22 +0700

    f1 f2

--- 05.py
+++ 05.py
@@ -1,10 +1,8 @@
 """
-Use gradient descent to find the minimum of the function f(x)=x*x + 2*x + 5
+Use gradient descent to find the minimum of the function f(x)
 """
 
-import numpy as np
 import matplotlib.pyplot as plt
-from numpy.typing import NDArray
 from abc import ABC, abstractmethod
 
 class Func(ABC):
@@ -13,12 +11,20 @@ class Func(ABC):
     @abstractmethod
     def calculate(self, weights: list[float]) -> list[float]:
         pass
+    def get_output(self, weights_arr: list[list[float]]) -> list[list[float]]:
+        return list(map(lambda weights:self.calculate(weights), weights_arr))
 
-class F(Func):
+class F1(Func):
     def calculate(self, weights: list[float]) -> list[float]:
         x, = weights
         a, b, c = self.inputs
         return [a*x*x + b*x + c]
+
+class F2(Func):
+    def calculate(self, weights: list[float]) -> list[float]:
+        x, = weights
+        a, b = self.inputs
+        return [abs(x+a) + b]
 
 def gradient_descent(f: Func) -> float:
     x: float = 0
@@ -31,12 +37,22 @@ def gradient_descent(f: Func) -> float:
     return x
 
 def main():
-    x: NDArray[np.float64] = np.array(np.arange(-20, 20), dtype="float64")
-    plt.plot(x, x*x + 2*x + 5)
-    f = F(inputs=[1, 2, 5])
-    x_point: float = gradient_descent(f)
-    plt.scatter([x_point], f.calculate([x_point]), color='red')
-    plt.title("f(x)=x*x + 2*x + 5")
+    x: list[list[float]] = list(map(lambda x: [x], list(range(-20, 20))))
+
+    _, axes = plt.subplots(1, 2, figsize=(10, 5))
+
+    f1 = F1(inputs=[1, 2, 5])
+    axes[0].plot(x, f1.get_output(x))
+    x_point: float = gradient_descent(f1)
+    axes[0].scatter([x_point], f1.calculate([x_point]), color='red')
+    axes[0].set_title("f(x) = x*x + 2*x + 5")
+
+    f2 = F2(inputs=[10, 4])
+    axes[1].plot(x, f2.get_output(x))
+    x_point: float = gradient_descent(f2)
+    axes[1].scatter([x_point], f2.calculate([x_point]), color='red')
+    axes[1].set_title("f(x) = 2*x*x*x + 3*x*x + 6*x + 2")
+
     plt.show()
 
 if __name__ == "__main__":
