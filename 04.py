f978fc1790980d1cd16a2b094ccd29bcd67326e8
klxcoder <klxcoder@gmail.com>
2025-04-05 19:26:54 +0700

    self.inputs

--- 04.py
+++ 04.py
@@ -8,42 +8,34 @@ from numpy.typing import NDArray
 from abc import ABC, abstractmethod
 
 class Func(ABC):
+    def __init__(self, inputs: list[float]):
+        self.inputs: list[float] = inputs
     @abstractmethod
-    def calculate(self, weights: list[float], inputs: list[float]) -> list[float]:
-        """
-        Calculate the result based on the given weights and inputs.
-
-        Args:
-            weights (List[float]): Change frequently, not fixed
-            inputs (List[float]): Fixed, do not change
-
-        Returns:
-            List[float]: A list containing the outputs of the calculation.
-        """
+    def calculate(self, weights: list[float]) -> list[float]:
         pass
 
 class F(Func):
-    def calculate(self, weights: list[float], inputs: list[float]) -> list[float]:
+    def calculate(self, weights: list[float]) -> list[float]:
         x, = weights
-        a, b, c = inputs
+        a, b, c = self.inputs
         return [a*x*x + b*x + c]
 
-def gradient_descent(f: Func, a: float, b: float, c: float) -> float:
+def gradient_descent(f: Func) -> float:
     x: float = 0
     times: int = 1000
     alpha: float = 0.01
     small: float = 0.01
     for _ in range(times):
-        dloss_dx: float = (f.calculate(weights=[x+small], inputs=[a, b, c])[0] - f.calculate(weights=[x], inputs=[a, b, c])[0])/small
+        dloss_dx: float = (f.calculate(weights=[x+small])[0] - f.calculate(weights=[x])[0])/small
         x -= alpha * dloss_dx
     return x
 
 def main():
     x: NDArray[np.float64] = np.array(np.arange(-20, 20), dtype="float64")
     plt.plot(x, x*x + 2*x + 5)
-    f = F()
-    x_point = gradient_descent(f, 1, 2, 5)
-    plt.scatter([x_point], f.calculate(weights=[x_point], inputs=[1, 2, 5]), color='red')
+    f = F(inputs=[1, 2, 5])
+    x_point: float = gradient_descent(f)
+    plt.scatter([x_point], f.calculate(weights=[x_point]), color='red')
     plt.title("f(x)=x*x + 2*x + 5")
     plt.show()
 
