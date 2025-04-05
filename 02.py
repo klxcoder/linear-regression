8c909fcc0a18471cf1bf01241bcfa058ad6de0c8
klxcoder <klxcoder@gmail.com>
2025-04-05 18:38:12 +0700

    f(a, b, c, x)

--- 02.py
+++ 02.py
@@ -5,7 +5,11 @@ Use gradient descent to find the minimum of the function f(x)=x*x + 2*x + 5
 import numpy as np
 import matplotlib.pyplot as plt
 
+def f(a: float, b: float, c: float, x: float) -> float:
+    return a*x*x + b*x + c
+
 def main():
+    print(f(1, 2, 5, 2)) # 13
     x = np.arange(-20, 20)
     y = x*x + 2*x + 5
     plt.plot(x, y)
