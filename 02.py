acb1e6c94db8e1c7b6578b01f01a0e81eef398d5
klxcoder <klxcoder@gmail.com>
2025-04-05 18:26:53 +0700

    import numpy and matplotlib

--- 02.py
+++ 02.py
@@ -2,8 +2,12 @@
 Use gradient descent to find the minimum of the function f(x)=x*x + 2*x + 5
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 def main():
-    pass
+    plt.plot(np.arange(10)**2)
+    plt.show()
 
 if __name__ == "__main__":
     main()
