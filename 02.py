a072c8b9561c0f901ed30a2cf55e80b05d0ba31b
klxcoder <klxcoder@gmail.com>
2025-04-05 18:54:39 +0700

    found x make f(x) minimal

--- 02.py
+++ 02.py
@@ -4,28 +4,27 @@ Use gradient descent to find the minimum of the function f(x)=x*x + 2*x + 5
 
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.typing import NDArray
 
 def f(a: float, b: float, c: float, x: float) -> float:
     return a*x*x + b*x + c
 
-def gradient_descent() -> tuple[float, float, float]:
-    a, b, c = 0, 0, 0
+def gradient_descent(a: float, b: float, c: float) -> float:
+    x: float = 0
     times: int = 1000
     alpha: float = 0.01
+    small: float = 0.01
     for _ in range(times):
-        dloss_da: float = 1
-        dloss_db: float = 1
-        dloss_dc: float = 1
-        a -= alpha * dloss_da
-        b -= alpha * dloss_db
-        c -= alpha * dloss_dc
-    return a, b, c
+        dloss_dx: float = (f(a, b, c, x+small) - f(a, b, c, x))/small
+        x -= alpha * dloss_dx
+    return x
 
 def main():
-    x = np.arange(-20, 20)
+    x: NDArray[np.float64] = np.array(np.arange(-20, 20), dtype="float64")
     plt.plot(x, x*x + 2*x + 5)
-    a, b, c = gradient_descent()
-    plt.plot(x, a*x*x + b*x + c)
+    x_point = gradient_descent(1, 2, 5)
+    plt.scatter(x_point, f(1, 2, 5, x_point), color='red')
+    plt.title("f(x)=x*x + 2*x + 5")
     plt.show()
 
 if __name__ == "__main__":
