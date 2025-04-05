8bfeb5556a21271d1141dceb81737c052daf5c56
klxcoder <klxcoder@gmail.com>
2025-04-05 18:43:27 +0700

    gradient_descent

--- 02.py
+++ 02.py
@@ -8,11 +8,24 @@ import matplotlib.pyplot as plt
 def f(a: float, b: float, c: float, x: float) -> float:
     return a*x*x + b*x + c
 
+def gradient_descent() -> tuple[float, float, float]:
+    a, b, c = 0, 0, 0
+    times: int = 1000
+    alpha: float = 0.01
+    for _ in range(times):
+        dloss_da: float = 1
+        dloss_db: float = 1
+        dloss_dc: float = 1
+        a -= alpha * dloss_da
+        b -= alpha * dloss_db
+        c -= alpha * dloss_dc
+    return a, b, c
+
 def main():
-    print(f(1, 2, 5, 2)) # 13
     x = np.arange(-20, 20)
-    y = x*x + 2*x + 5
-    plt.plot(x, y)
+    plt.plot(x, x*x + 2*x + 5)
+    a, b, c = gradient_descent()
+    plt.plot(x, a*x*x + b*x + c)
     plt.show()
 
 if __name__ == "__main__":
