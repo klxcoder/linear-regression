04e350737ae74059fcf81ecfee9776731c9e5858
klxcoder <klxcoder@gmail.com>
2025-04-05 19:11:41 +0700

    class Func(ABC) use @abstractmethod

--- 04.py
+++ 04.py
@@ -5,25 +5,38 @@ Use gradient descent to find the minimum of the function f(x)=x*x + 2*x + 5
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy.typing import NDArray
+from abc import ABC, abstractmethod
 
-def f(a: float, b: float, c: float, x: float) -> float:
-    return a*x*x + b*x + c
+class Func(ABC):
+    @abstractmethod
+    def calculate(self, weights: list[float], inputs: list[float]) -> list[float]:
+        pass
 
-def gradient_descent(a: float, b: float, c: float) -> float:
+class F(Func):
+    def calculate(self, weights: list[float], inputs: list[float]) -> list[float]:
+        a, b, c = weights
+        x, = inputs
+        return [a*x*x + b*x + c]
+
+# def f(a: float, b: float, c: float, x: float) -> float:
+#     return a*x*x + b*x + c
+
+def gradient_descent(f: Func, a: float, b: float, c: float) -> float:
     x: float = 0
     times: int = 1000
     alpha: float = 0.01
     small: float = 0.01
     for _ in range(times):
-        dloss_dx: float = (f(a, b, c, x+small) - f(a, b, c, x))/small
+        dloss_dx: float = (f.calculate([a, b, c], [x+small])[0] - f.calculate([a, b, c], [x])[0])/small
         x -= alpha * dloss_dx
     return x
 
 def main():
     x: NDArray[np.float64] = np.array(np.arange(-20, 20), dtype="float64")
     plt.plot(x, x*x + 2*x + 5)
-    x_point = gradient_descent(1, 2, 5)
-    plt.scatter(x_point, f(1, 2, 5, x_point), color='red')
+    f = F()
+    x_point = gradient_descent(f, 1, 2, 5)
+    plt.scatter([x_point], f.calculate([1, 2, 5], [x_point]), color='red')
     plt.title("f(x)=x*x + 2*x + 5")
     plt.show()
 
